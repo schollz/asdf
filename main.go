36b407d9a1bbed53a077987e52f3297e655f749e
Zack Scholl <zack.scholl@gmail.com>
2024-10-31 07:30:06 -0700

    add workflows

--- main.go
+++ main.go
@@ -2,17 +2,25 @@ package main
 
 import (
 	"flag"
+	"fmt"
 
 	"github.com/schollz/asdf/src/runner"
 )
 
 var flagFilename string
+var flagVersion bool
+var Version string
 
 func init() {
+	flag.BoolVar(&flagVersion, "version", false, "print version")
 	flag.StringVar(&flagFilename, "filename", "", "filename to run")
 }
 
 func main() {
 	flag.Parse()
+	if flagVersion {
+		fmt.Println("asdf version", Version)
+		return
+	}
 	runner.Run(flagFilename)
 }
