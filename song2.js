ba9f2d0e6b168d0f1988ece485492c3c830d6ef3
Zack Scholl <zack.scholl@gmail.com>
2024-11-05 14:57:38 -0800

    ok

--- song2.js
+++ song2.js
@@ -1,27 +1,22 @@
-bpm = 160
-part1 = `
-.bpm${bpm}.gate50
-c4 a d5 
-c5 e g5
-d5 g5 c5
-c6 e f4 c6 e f4
-c2 b1 g2 g1
+part3 = `
+c4.bpm40.attack70.release100.velocity50.gate50.reverb70.detune70 d
+e4.detune30,40,80 d.mix30,70
+d4.detune50 g4.mix20,90.velocity60
+c5 - b4
 `
 
-part2 = `.gate50.bpm120.velocity30.reverb0
-C;3.arp.up8.down6.up2.down4
-Em;3.arp.up8.down6.up2.down4
-Am/E;3.arp.up8.down6.up2.down4
-F;3.arp.up8.down6.up2.down4
+chords = `
+Am;2.velocity40.gate70,60,30,50.release200,150,170.reverb40.attack50
+F/A;2.gate70,60,30,50.release200,150,170
+C;2.gate30,50,60,30,50.release140,110,170
+Em/B;2.gate80,90,60,30,50.release70,200,150,170
 `
 
-part3 = `c5.velocity120.gate50
-d e
-c5
-g f
+kick = `
+.bpm30.reverb30
+c4.prob90.velocity40,35,50.release50,100,200.gate50,100 -  ~
 `
 
-supercollider_polyperc_1 = "part2"
-
-supercollider_polyperc_2 = "part3"
-
+supercollider_volcano="kick"
+supercollider_jp_1 = "chords"
+supercollider_jp_2 = "part3"
