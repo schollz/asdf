725592c9112f3b688df9b1f865e1776faa9e9864
Zack <zack.scholl@gmail.com>
2024-11-14 13:28:28 -0800

    fix for crow

--- song1.js
+++ song1.js
@@ -11,10 +11,25 @@ Em/B;3.arp.u6,9,12.d8.u12.d4
 Am/E;3.arp.u4.d6.u9.d5
 F/C;3.arp.u6.d2.t1.u8.d4
 `
+bpm=150
+octave=4
+part3 = `.bpm${bpm}
+Cmaj/B;${octave}.arp.up8.down8
+Em/B;${octave}.arp.up8.down8
+Am/E;${octave}.arp.up8.down8
+F/C;${octave}.arp.up8.down8
+`
+part4 = `.bpm${bpm}
+Cmaj/B;${octave}.arp.up8.down8
+`
 
-part3 = "C.arp.u12.d8"
+clock=`.bpm${bpm}.beat1
+(c3 c7) * 8`
+
 //part2 = "a.bpm90.attack10 b c d"
 // midi_op1 = "part1"
 
-supercollider_polyperc_1="part1"
+crow_voct1 = "clock"
+crow_voct2 = "part3"
+//supercollider_polyperc_1="part1"
 //debug = "part3"
